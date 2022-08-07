016d9302df63ac267d43716823a32eb3f7942fcb
TkachenokPavel <pasha.tkachenok@mail.ru>
2022-08-07 17:07:23 +0300

    feat(hw-39): add hooks

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { StyledSearch } from './styles'
 
 export const Search = () => {
     return (
-        <div>Search</div>
+        <StyledSearch
+            type='text'
+            placeholder='search...'
+            name='search'
+        />
     )
 }
--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,1 +1,13 @@
-export { }
+import { useCallback, useState } from "react"
+
+export const useToggle = (initialValue: boolean = false): [boolean, () => void] => {
+    const [value, setValue] = useState<boolean>(initialValue);
+
+    const toggle = useCallback(
+        () => {
+            setValue(value => !value);
+        }, [],
+    )
+
+    return [value, toggle]
+}
